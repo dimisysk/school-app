4064dafd41a5c5b168e1fe7a6b5b8904961c61f1
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-05-06 02:28:00 +0200

    ITeacher DAO

--- src/main/java/gr/aueb/cf/schoolapp/dao/ITeacherDAO.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/ITeacherDAO.java
@@ -1,15 +1,14 @@
 package gr.aueb.cf.schoolapp.dao;
 
-import gr.aueb.cf.schoolapp.dao.exception.TeachersDaoException;
+import gr.aueb.cf.schoolapp.dao.exceptions.TeacherDAOException;
 import gr.aueb.cf.schoolapp.model.Teacher;
 
 import java.util.List;
 
 public interface ITeacherDAO {
-
-    Teacher insert(Teacher teacher) throws TeachersDaoException;
-    Teacher update(Teacher teacher) throws TeachersDaoException;
-    void delete(Integer id) throws TeachersDaoException;
-    Teacher getById(Integer id) throws TeachersDaoException;
-    List<Teacher> getByLastname(String lastname) throws TeachersDaoException;
+    Teacher insert(Teacher teacher) throws TeacherDAOException;
+    Teacher update(Teacher teacher) throws TeacherDAOException;
+    void delete(Integer id) throws TeacherDAOException;
+    Teacher getById(Integer id) throws TeacherDAOException;
+    List<Teacher> getByLastname(String lastname) throws TeacherDAOException;
 }
