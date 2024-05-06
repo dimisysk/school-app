f2f5bb1bf2acfc42a873a3ae919aff15f200a75a
Dimi Syskakis <Syskakisdimitrios@yahoo.com>
2024-05-06 02:28:48 +0200

    Teacher exception

--- src/main/java/gr/aueb/cf/schoolapp/dao/exception/TeachersDaoException.java
+++
@@ -1,9 +0,0 @@
-package gr.aueb.cf.schoolapp.dao.exception;
-
-public class TeachersDaoException extends Exception{
-     private static final long serialVersionUID = 1L;
-
-     public TeachersDaoException(String str) {
-         super(str);
-     }
-}
